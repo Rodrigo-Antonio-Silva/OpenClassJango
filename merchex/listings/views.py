f45ea229698a4a94fde86630338e90cfa4bd0b93
Rodrigo <rodrigo2013.rs33@gmail.com>
2024-01-08 01:37:22 -0300

    Adição de 4 pag.

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def hello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>About us</h1> <p>We love merch!</p>')
+
+
+def list(request):
+    return HttpResponse('<ul>Bandas<il>Foo fighters</il><il>Metallica</il></ul>')
+
+
+def contact(request):
+    return HttpResponse('<p>Tel: 555-5555</h1>')
